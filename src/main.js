c04acd2ee2905c0284860f8c7bac303cd87f84fa
whien <sal95610@gmail.com>
2016-03-29 18:41:09 +0800

    test work

--- src/main.js
+++ src/main.js
@@ -3,24 +3,39 @@ import ReactDom, { render } from 'react-dom';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import 'react-bootstrap';
 // products will be presented by react-bootstrap-table
-var products = [{
-      id: 1,
-      name: "Item name 1",
-      price: 100
+const contacts = [{
+      component: 1,
+      release: "Item name 1",
+      email: 100,
+      role: 1
   },{
-      id: 2,
-      name: "Item name 2",
-      price: 100
+      component: 2,
+      release: "Item name 2",
+      email: 100,
+      role: 2
   }];
 // It's a data format example.
 function priceFormatter(cell, row){
   return '<i class="glyphicon glyphicon-usd"></i> ' + cell;
 }
 
-React.render(
-    <BootstrapTable data={products} striped={true} hover={true} insertRow={true} deleteRow={true} >
-        <TableHeaderColumn dataField="id" isKey={true} dataAlign="center" dataSort={true} editable={false}>Product ID</TableHeaderColumn>
-        <TableHeaderColumn dataField="name" dataSort={true}>Product Name</TableHeaderColumn>
-        <TableHeaderColumn dataField="price" dataFormat={priceFormatter}>Product Price</TableHeaderColumn>
+const tableOptions = {
+
+}
+
+render(
+    <BootstrapTable data={contacts} insertRow={true} deleteRow={true} options={tableOptions}>
+      <TableHeaderColumn dataField="component" dataAlign="center" isKey={true} editable={false}>
+        Component
+      </TableHeaderColumn>
+      <TableHeaderColumn dataField="release" dataAlign="center" editable={false}>
+        Release
+      </TableHeaderColumn>
+      <TableHeaderColumn dataField="email" dataAlign="center">
+        Email
+      </TableHeaderColumn>
+      <TableHeaderColumn dataField="role" dataAlign="center">
+        Contact Role
+      </TableHeaderColumn>
     </BootstrapTable>, document.getElementById("app")
 );
